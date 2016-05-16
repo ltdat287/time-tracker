c760e8a3678cd886b24cbe630f4a31cb45854daf
TienDat <datlt@mog.net>
2016-05-16 15:02:19 +0700

    Finish font-end build in angular js with example data_base in file time.json

--- scripts/services/time.js
+++ scripts/services/time.js
@@ -17,14 +17,46 @@
                 // $promise.then allows us to intercept the results
                 // which we will use later
                 return Time.query().$promise.then(function(results) {
+                    angular.forEach(results, function(result) {
+                        // Add the loggedTime property which calls getTimeDiff to give us a duration object
+                        result.loggedTime = getTimeDiff(result.start_time, result.end_time);
+                    });
                     return results;
                 }, function(error) { // Check for errors
                     console.log(error);
                 });
             }
 
+            // Use Moment.js to get the duration of the time entry
+            function getTimeDiff(start, end) {
+                var diff = moment(end).diff(moment(start));
+                var duration = moment.duration(diff);
+
+                return {
+                    duration: duration
+                }
+            }
+
+            // Add up the total time for all of our time entries
+            function getTotalTime(timeentries) {
+                var totalMilliseconds = 0;
+
+                angular.forEach(timeentries, function(key) {
+                    totalMilliseconds += key.loggedTime.duration._milliseconds;
+                });
+                // After 24 hours, the Moment.js duration object reports the next unit up, which is days.
+                // Using the asHours method and rounding down with Math.floor instead us the toal hours
+                return {
+                    hours: Math.floor(moment.duration(totalMilliseconds).asHours()),
+                    minutes: moment.duration(totalMilliseconds).minutes()
+                }
+
+            }
+
             return {
                 getTime: getTime,
+                getTimeDiff: getTimeDiff,
+                getTotalTime: getTotalTime
             }
         }
             
